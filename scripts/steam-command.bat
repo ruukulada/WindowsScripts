991e2adc577aa792e89aaac957c9defdfd1e9c07
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-05-13 21:08:40 -0400

    Update steam-command.bat

--- scripts/steam-command.bat
+++ scripts/steam-command.bat
@@ -1,6 +1,6 @@
 :: see the output of %COMMAND% from steam launch options
 :: syntax: "%UserProfile%\Documents\GitHub\WindowsScripts\scripts\steam-command.bat" %COMMAND%
 
-echo off
+@echo off
 echo %1
 pause
