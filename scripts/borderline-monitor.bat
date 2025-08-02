c7a28f44156cf3fdc35f8c63bd551b359584c897
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-08-02 02:07:24 -0400

    Update borderline-monitor.bat

--- scripts/borderline-monitor.bat
+++ scripts/borderline-monitor.bat
@@ -1,5 +1,9 @@
 :: start borderline.exe to bring all windows to target monitor
 
 @echo off
-set /p monitorNumber=Enter monitor number: 
+if "%~1"=="" (
+    set /p monitorNumber=Enter monitor number: 
+) else (
+    set monitorNumber=%~1
+)
 start "" "%ProgramFiles%\Borderline\Borderline.exe" %monitorNumber%
