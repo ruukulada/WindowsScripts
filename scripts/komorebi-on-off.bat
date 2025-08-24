9ff423db70cff5d7ee75d0f31b5a38fce58c1205
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-08-24 15:27:29 -0400

    Komorebi config

--- scripts/komorebi-on-off.bat
+++ scripts/komorebi-on-off.bat
@@ -5,7 +5,7 @@ set EXE_NAME=komorebi.exe
 
 tasklist /FI "IMAGENAME eq %EXE_NAME%" 2>NUL | find /I "%EXE_NAME%" >NUL
 if %ERRORLEVEL%==0 (
-    komorebic stop
+    komorebic stop --whkd
 ) else (
-    komorebic start
+    komorebic start --whkd
 )
