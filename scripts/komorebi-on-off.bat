2677ec7fc8cf71a72d860def7ffefe07d9a05f20
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-09-06 14:15:15 -0400

    Update komorebi-on-off.bat

--- scripts/komorebi-on-off.bat
+++ scripts/komorebi-on-off.bat
@@ -5,7 +5,7 @@ set EXE_NAME=komorebi.exe
 
 tasklist /FI "IMAGENAME eq %EXE_NAME%" 2>NUL | find /I "%EXE_NAME%" >NUL
 if %ERRORLEVEL%==0 (
-  komorebic stop --whkd
+  "%ProgramFiles%\komorebi\bin\komorebic-no-console.exe" stop --whkd
 ) else (
-  komorebic start --whkd
+  "%ProgramFiles%\komorebi\bin\komorebic-no-console.exe" start --whkd
 )
