32a60303691263248529aa834376618b6b11e9b0
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-09-16 18:13:04 -0400

    Update komorebi-on-off.bat

--- scripts/komorebi-on-off.bat
+++ scripts/komorebi-on-off.bat
@@ -1,8 +1,14 @@
-:: open komorebi if it is closed, close it if is open
+:: open komorebi if it is closed, close it if is open, run as admin (sudo)
 
 @echo off
-set EXE_NAME=komorebi.exe
+setlocal
+net session >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+  sudo "%~f0" %*
+  exit /b
+)
 
+set EXE_NAME=komorebi.exe
 tasklist /FI "IMAGENAME eq %EXE_NAME%" 2>NUL | find /I "%EXE_NAME%" >NUL
 if %ERRORLEVEL%==0 (
   "%ProgramFiles%\komorebi\bin\komorebic-no-console.exe" stop --whkd
