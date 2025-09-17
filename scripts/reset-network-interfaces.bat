425ac5813ec582d0a89c993658b1b8f5271b1400
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-09-17 18:10:54 -0400

    Update reset-network-interfaces.bat

--- scripts/reset-network-interfaces.bat
+++ scripts/reset-network-interfaces.bat
@@ -1,10 +1,17 @@
-:: disable and re-enable network interfaces
-:: requires admin privileges
+:: disable and re-enable network interfaces, run as admin (sudo)
+
+@echo off
+setlocal
+net session >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+  sudo "%~f0" %*
+  exit /b
+)
 
 @echo off
 for /f "tokens=3,* delims= " %%A in ('netsh interface show interface ^| find /i "connected"') do (
-    echo Resetting: %%B
-    netsh interface set interface %%B DISABLED
-    timeout /t 2
-    netsh interface set interface %%B ENABLED
+  echo Resetting: %%B
+  netsh interface set interface %%B DISABLED
+  timeout /t 2
+  netsh interface set interface %%B ENABLED
 )
