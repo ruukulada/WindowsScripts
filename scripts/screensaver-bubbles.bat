8138e78c9d8560969b0fc452bc3fb5e2d080ec47
ruukulada <126130342+ruukulada@users.noreply.github.com>
2025-09-17 11:59:01 -0400

    Update screensaver-bubbles.bat

--- scripts/screensaver-bubbles.bat
+++ scripts/screensaver-bubbles.bat
@@ -1,4 +1,7 @@
-:: start bubbles screensaver
+:: start bubbles screensaver every 30 seconds
 
 timeout /t 1
+:start
 %SystemRoot%\System32\bubbles.scr -start
+timeout /t 30 /nobreak >NUL
+goto start
