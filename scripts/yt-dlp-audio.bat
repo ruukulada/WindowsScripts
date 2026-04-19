cd23487283be20c981ea2c3e014ae70c4c235037
ruukulada <126130342+ruukulada@users.noreply.github.com>
2026-04-19 00:06:33 -0700

    Update yt-dlp-audio.bat

--- scripts/yt-dlp-audio.bat
+++ scripts/yt-dlp-audio.bat
@@ -6,13 +6,20 @@
 :: m4a best audio
 
 @echo off
+set /a output=%RANDOM%
 cd "%UserProfile%\Downloads"
+mkdir "%output%"
+cd "%output%"
 echo Paste YouTube URL:
 set /p url=
-yt-dlp^
-  -x %url%
-  -f ba[ext=m4a]^
+yt-dlp -v ^
+  %url% ^
+  -x ^
+  -f bestaudio^
+  --audio-format m4a^
+  --parse-metadata "%%(playlist_index)s:%%(track_number)s"^
+  --parse-metadata "%%(artists.0)s:%%(meta_artist)s"^
+  --embed-metadata^
   --embed-thumbnail^
   --convert-thumbnail jpg^
-  --ppa "ThumbnailsConvertor+FFmpeg_o:-c:v mjpeg -vf crop=\"'"'"'if(gt(ih,iw),iw,ih)'"'"':'"'"'if(gt(iw,ih),ih,iw)'"'"'\""^
-  --embed-metadata
+  --ppa "ThumbnailsConvertor+FFmpeg_o:-c:v mjpeg -vf crop=\"'"'"'if(gt(ih,iw),iw,ih)'"'"':'"'"'if(gt(iw,ih),ih,iw)'"'"'\""
