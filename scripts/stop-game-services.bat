9ecd203d4503e637868cd88f405271b8c2381607
ruukulada <126130342+ruukulada@users.noreply.github.com>
2026-06-22 15:50:53 -0700

    Update stop-game-services.bat

--- scripts/stop-game-services.bat
+++ scripts/stop-game-services.bat
@@ -4,5 +4,6 @@ taskkill/im EpicGamesLauncher.exe
 taskkill/im EADesktop.exe
 timeout /t 2 /nobreak >NUL
 net stop EABackgroundService
+net stop PnkBstrA
 taskkill/im upc.exe
 taskkill/im Battle.net.exe
